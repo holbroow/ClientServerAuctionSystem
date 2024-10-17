85d31115bd671c483a8b14a0a120e784b6a9a166
holbroow <willholbrook10@gmail.com>
2024-10-17 18:07:30 +0100

    Rectified incorrect start.sh shekll script. Project ready for submission.

--- partA/start.sh
+++ partA/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "cd \"$(pwd)/$server\" && rmiregistry; exec bash"
-gnome-terminal -- bash -c "cd \"$(pwd)/$server\" && java Server; exec bash"
-gnome-terminal -- bash -c "cd \"$(pwd)/$client\"; exec bash"
+gnome-terminal -- bash -c "cd \"$(pwd)/server\" && rmiregistry; exec bash"
+gnome-terminal -- bash -c "cd \"$(pwd)/server\" && java Server; exec bash"
+gnome-terminal -- bash -c "cd \"$(pwd)/client\"; exec bash"
