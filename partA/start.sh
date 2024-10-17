1a573f189b6bd489d236b2e1791fa7b99e0452b3
holbroow <willholbrook10@gmail.com>
2024-10-17 18:12:25 +0100

    Another revision to the shell script as per moodle instructions.

--- partA/start.sh
+++ partA/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gnome-terminal -- bash -c "cd \"$(pwd)/server\" && rmiregistry; exec bash"
+sleep 2
 gnome-terminal -- bash -c "cd \"$(pwd)/server\" && java Server; exec bash"
-gnome-terminal -- bash -c "cd \"$(pwd)/client\"; exec bash"
